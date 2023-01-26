433947f2fe7571d377df2d5305c02b35e24018a4
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2023-01-26 10:24:10 -0300

    Criando form

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -22,12 +22,14 @@ export const styles = StyleSheet.create({
     },
 
     input: {
+        flex: 1,
         height: 56,
         backgroundColor: '#1F1E25',
         borderRadius: 5,
         color: '#FFF',
         padding: 16,
         fontSize: 16,
+        marginRight: 12,
     },
 
     buttonText: {
@@ -42,5 +44,12 @@ export const styles = StyleSheet.create({
         backgroundColor: '#31CF67',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+
+    form: {
+        width: '100%',
+        flexDirection: 'row',
+        marginTop: 36,
+        marginBottom: 42,
     }
 });
